ab68b6648191f7dcda78c61cb5a7af427124ace2
aguirre739 <pame11_@live.com>
2020-09-09 16:37:57 -0300

    primeras configuraciones

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+// const express = require("express");
+import morgan from "morgan";
+// const morgan = require("morgan");
+import cors from "cors";
+// const cors = require('cors');
+import path from "path";
+
+
+//configuracion basica
+const app = express(); //creamos una instancia de express
+//crear una variable port
+app.set('port', process.env.PORT || 4000)
+
+// const port = 4000;
+
+app.listen(app.get('port'), () =>{
+    console.log(path.join(__dirname, "../public"));
+    console.log("estoy escuchando el puerto "+app.get('port'));
+})
+
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+//lo siquiente es para que entienda a los JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//aqui agrego la carpeta public con los archivos estaticos
+app.use(express.static(path.join(__dirname, "../public")))
+
+app.get('/', (req, res) =>{
+    res.send("Hola Mundo")
+});
+
+app.get('/usuarios', (req, res)=>{
+    res.send("estoy en la pagina de usuarios")
+})
